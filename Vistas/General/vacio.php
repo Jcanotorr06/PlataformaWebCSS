4ac4a33a0772863b2f1b0cf291ad9e943bcbe340
Jcanotorr06 <jcano062001@gmail.com>
2021-12-05 13:47:28 -0500

    Añadir footer a la pagina de inicio vacia

--- Vistas/General/vacio.php
+++ Vistas/General/vacio.php
@@ -6,3 +6,19 @@
     <h3>No tiene citas agendadas</h3>
     <a href="/agendar" class="btn px-3 px-lg-5 py-lg-3 btn-lg btn-primary text-white rounded-pill">Agendar Cita</a>
 </main>
+<footer class="w-100 fixed-bottom navbar-dark bg-dark">
+    <nav class="container py-3">
+        <div class="row">
+            <div class="col-md-6 d-flex align-items-center">
+                <div >
+                    <p class=" navbar-text">Caja de Seguro Social © 2021. Todos los derechos reservados.</p>
+                </div>
+            </div>
+            <div class="col-md-6 d-flex justify-content-end align-items-center">
+                <a href="https://w3.css.gob.pa/">
+                    <img src="https://w3.css.gob.pa/wp-content/uploads/2020/09/cropped-cajalogo-2.png" alt="logo css" id="footerLogo">
+                </a>
+            </div>
+        </div>
+    </nav>
+</footer>
