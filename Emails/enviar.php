776c7766382789efcfdb27b20d78819677123725
Jcanotorr06 <jcano062001@gmail.com>
2021-12-10 18:15:24 -0500

    Actualizar script de envio

--- Emails/enviar.php
+++ Emails/enviar.php
@@ -3,7 +3,7 @@
 function enviarEmail($to, $subject, $body){
     $headers  = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
-    $headers .= "From: Plataforma Web CSS notificaciones@csspanama.online" . "\r\n";
+    $headers .= "From: notificaciones@csspanama.online" . "\r\n";
     return mail($to, $subject, $body, $headers);
 }
 
