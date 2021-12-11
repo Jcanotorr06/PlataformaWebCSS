8b55f14bd0f5d9f71592309f7a1802f42b28f7fe
Jcanotorr06 <jcano062001@gmail.com>
2021-12-11 07:47:52 -0500

    Modificar funcion de envio de emails

--- Emails/enviar.php
+++ Emails/enviar.php
@@ -5,7 +5,8 @@ function enviarEmail($to, $subject, $body){
     $headers  = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
     $headers .= "From: notificaciones@csspanama.online" . "\r\n";
-    return exec("echo '$body' | mail -a 'MIME-Version: 1.0\r\nContent-type: text/html; charset=UTF-8\r\nFrom: notificaciones@csspanama.online' -r 'notificaciones@csspanama.online' -s '$subject' $to");
+    exec("echo '$body' | mail -a 'MIME-Version: 1.0\r\nContent-type: text/html; charset=UTF-8\r\nFrom: notificaciones@csspanama.online' -r 'notificaciones@csspanama.online' -s '$subject' $to");
+    return true;
 }
 
 ?>
