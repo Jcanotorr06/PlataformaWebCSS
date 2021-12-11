42a82c532b4164b3bb47a78eb29154e1c4457f0c
Jcanotorr06 <jcano062001@gmail.com>
2021-12-10 22:02:32 -0500

    Actualizar script de envio

--- Emails/enviar.php
+++ Emails/enviar.php
@@ -4,7 +4,7 @@ function enviarEmail($to, $subject, $body){
     $headers  = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
     $headers .= "From: notificaciones@csspanama.online" . "\r\n";
-    exec("echo '$body' | mail -a '$headers' -s '$subject' $to");
+    exec("echo '$body' | mail -a 'MIME-Version: 1.0\r\nContent-type: text/html; charset=UTF-8\r\nFrom: notificaciones@csspanama.online' -r 'notificaciones@csspanama.online' -s '$subject' $to");
     return mail($to, $subject, $body, $headers); 
 }
 
